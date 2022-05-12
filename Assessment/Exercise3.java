a7434fc5550c80053758344c524015b3f99f1d1f
Vi1234sh12 <vishald41234@gmail.com>
2022-05-12 13:44:27 +0530

    add code Command line

--- Assessment/Exercise3.java
+++ Assessment/Exercise3.java
@@ -1,7 +1,7 @@
 public class Exercise3 {
 
     public static void main(String[] args) {
-        System.out.println("Enter the Number : ");
+        System.out.println("Command Line arguments " + args[0] + " " + args[1] + " " + args[2]);
 
     }
 
