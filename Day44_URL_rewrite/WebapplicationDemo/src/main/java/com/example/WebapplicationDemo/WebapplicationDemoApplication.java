d09f9b93ca9021f78b51311dd2475f96649ccbd3
Vi1234sh12 <vishald41234@gmail.com>
2022-05-28 12:03:36 +0530

    Session Hnadling using URL rewritting

---
+++ Day44_URL_rewrite/WebapplicationDemo/src/main/java/com/example/WebapplicationDemo/WebapplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebapplicationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapplicationDemoApplication.class, args);
+	}
+
+}
