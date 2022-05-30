fa60c3d814006be787b26904a4dd1c1cb7f7d5aa
Vi1234sh12 <vishald41234@gmail.com>
2022-05-30 14:39:03 +0530

    Spring Security add user & password inside application-properties

---
+++ Spring_Security_User/SpringSecurityUser/src/main/java/com/example/SpringSecurityUser/SpringSecurityUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityUserApplication.class, args);
+	}
+
+}
---
+++ Spring_Security_User/SpringSecurityUser/src/test/java/com/example/SpringSecurityUser/SpringSecurityUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
